03f5acbafab8a8db0d6d6303020348d1ef79a2ab
derrickcrowis <derrickcrowis@gmail.com>
2023-03-27 22:16:18 -0700

    3-main.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
  */
-int main(void)
+void _puts(char *str)
 {
-    char *str;
+	while (*str)
+		_putchar(*str++);
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	_putchar('\n');
 }
