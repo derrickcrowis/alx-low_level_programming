bf316c5e8debfb801c87465ad4c2037fa313d072
derrickcrowis <derrickcrowis@gmail.com>
2023-04-18 18:59:29 -0700

    5-main.c

--- 0x0E-structures_typedef/5-main.c
+++ 0x0E-structures_typedef/5-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "dog.h"
+#include "../dog.h"
 
 /**
- * main - code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -10,8 +10,8 @@ int main(void)
 {
 	dog_t *my_dog;
 
-	my_dog = new_dog("Poppy", 3.5, "Jay");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	my_dog = new_dog("Django", 3.5, "Jay");
+	printf("Name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 	free_dog(my_dog);
 	return (0);
 }
